d864579eb66cff91aa4c3ef91f9895d49ab39ea3
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-03-20 20:12:43 +0000

    Create base structure of excuse generator html and js files

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const excuses = ["escusa1","escusa2","escusa3","escusa4","escusa5"]
+
+  let randomExcuseNumber =  Math.floor(Math.random() * excuses.length)
+
+  let randomExcuse = excuses[randomExcuseNumber]
+
+  // I need to create the h1 element in the DOM
+  let heading = document.createElement("h1")
+  heading.innerText = randomExcuse
+
+  const targetDiv = document.getElementById("randomDivContainer")
+
+  //Place the element with the excuse in the DOM, inside the heading tag
+  targetDiv.appendChild(heading)
+
 };
